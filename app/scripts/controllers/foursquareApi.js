ce2a74aea4fe1a589a40a05b77fc704bbdca192f
George Somerville <georgesomerville1992@gmail.com>
2015-03-16 23:55:02 +0000

    added title and image

--- app/scripts/controllers/foursquareApi.js
+++ app/scripts/controllers/foursquareApi.js
@@ -7,6 +7,11 @@ angular.module("myTestAppApp")
 		"CLIENT_SECRET":"FOEJEEY51KIIOGR01YNTUCP5NDTPPAOL0FXKJL5A3IGQEJPL",
 		"VERSION":"20130815"
 	})
+	.constant('FQRESULTS',{
+
+
+
+	})
 	.controller("attractionCtrl",function($scope){
 		$scope.savedLocations = [
 						{
@@ -19,28 +24,44 @@ angular.module("myTestAppApp")
 						}
 
 		]
+		$scope.savedAttractions = [
+
+				// this will have data pushed through
+				{
+					attractionName:"",
+					attractionImagePrefix:"",
+					attractionImageSuffix:""
+
+				}
+		]
 		// $scope.submitRequest()
 		// make request
-	})
+	})					// name            // dependencies
 	.controller("FoursquareCtrl",["$scope","$http","FOURSQUARE",function($scope,$http,FOURSQUARE){
 
-		var foursquareUrl = "https://api.foursquare.com/v2/venues/search?client_id="+FOURSQUARE.CLIENT_ID+"&client_secret="+FOURSQUARE.CLIENT_SECRET+"&v="+FOURSQUARE.VERSION
+		var foursquareUrl = "https://api.foursquare.com/v2/venues/explore?client_id="+FOURSQUARE.CLIENT_ID+"&client_secret="+FOURSQUARE.CLIENT_SECRET+"&v="+FOURSQUARE.VERSION
 		console.log(foursquareUrl)	
 		this.newSearch = {}
 		$scope.location = ""
-		$scope.attractionType = ""
+		$scope.attractions = ""
 		$scope.submitRequest = function(){
 			var query = {attractionType:$scope.attractionType,city:$scope.city}
 			$scope.savedLocations.push(query)
-
-
 			$scope.foursquare(query.attractionType,query.city)
 		}	
 		$scope.foursquare = function(attraction,city){
 			console.log("query.atttraction",attraction)
-			$http.get(foursquareUrl+"&near="+city+"&query="+attraction).success(function(data){
-				console.log(data)
-
+			$http.get(foursquareUrl+"&near="+city+"&query="+attraction+"&venuePhotos=1").success(function(data){
+				$scope.foursquareAttractions = data.response["groups"][0].items
+				console.log($scope.foursquareAttractions);
+				angular.forEach($scope.foursquareAttractions, function(value, key){
+					console.log(value)
+					var savedAttraction = {attractionName:value.venue.name,attractionImagePrefix:value.venue.photos.groups[0].items[0].prefix,attractionImageSuffix:value.venue.photos.groups[0].items[0].suffix}
+					$scope.savedAttractions.push(savedAttraction);
+					// return $scope.foursquareAttractions
+				});
+				// console.log(data.response["groups"][0].items)
+				console.log($scope.savedAttractions)
 				// use explore to get groups which includes photos
 				// $scope.products = data
 			})
