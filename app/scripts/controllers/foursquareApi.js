cf0a520b43c69734181b77a21efd768e970320ff
George Somerville <georgesomerville1992@gmail.com>
2015-03-17 21:54:15 +0000

    google maps is in place but needs refactoring so user can put in country in order to geoCode

--- app/scripts/controllers/foursquareApi.js
+++ app/scripts/controllers/foursquareApi.js
@@ -7,11 +7,19 @@ angular.module("myTestAppApp")
 		"CLIENT_SECRET":"FOEJEEY51KIIOGR01YNTUCP5NDTPPAOL0FXKJL5A3IGQEJPL",
 		"VERSION":"20130815"
 	})
-	.constant('FQRESULTS',{
-
-
-
+	.constant('GOOGLEGEOCODE',{
+		"APIKEY":"AIzaSyBDi56DVodoH92MNTpQfcPtloDx0y8CgY8",
+		"URL":"https://maps.googleapis.com/maps/geocode/output?parameters",
+		location:document.getElementById("geocodeCity")
 	})
+	// .constant('MAPOPTIONS',{
+	// 	zoom: 12,  // hostel based on what user has typed in we need to get corrdinated of what user as typed in
+	// 						// or find away where google will just geocode the city
+	// 						// because in my project I gecoded the cidy the user typed into latpoints.
+ //    // center: new google.maps.LatLng(),
+ //    mapTypeId: google.maps.MapTypeId.ROADMAP,
+ //    map_container:document.getElementById('map-canvas')
+	// })
 	.controller("attractionCtrl",function($scope){
 		$scope.savedLocations = [
 						{
@@ -30,22 +38,53 @@ angular.module("myTestAppApp")
 				{
 					attractionName:"",
 					attractionImagePrefix:"",
-					attractionImageSuffix:""
+					attractionImageSuffix:"",
+					attractionOpeningTimes:""
 
 				}
 		]
 		// $scope.submitRequest()
 		// make request
 	})					// name            // dependencies
-	.controller("FoursquareCtrl",["$scope","$http","FOURSQUARE",function($scope,$http,FOURSQUARE){
+	.controller("googleMapsController",["$scope","$http","MAPOPTIONS"],function($scope,$http,MAPOPTIONS){
+		  $scope.createMap = function(){
+	      if(MAPOPTIONS.map_container != undefined){
+	        window.map = new google.maps.Map(MAPOPTIONS.map_container, MAPOPTIONS) 
+	        console.log(window.map)
+	      }
+	    }
+
+	})
+
+
+	.controller("FoursquareCtrl",["$scope","$http","FOURSQUARE","GOOGLEGEOCODE",function($scope,$http,FOURSQUARE,GOOGLEGEOCODE){
 
 		var foursquareUrl = "https://api.foursquare.com/v2/venues/explore?client_id="+FOURSQUARE.CLIENT_ID+"&client_secret="+FOURSQUARE.CLIENT_SECRET+"&v="+FOURSQUARE.VERSION
-		console.log(foursquareUrl)	
+		console.log("geocode location")
 		this.newSearch = {}
 		$scope.location = ""
 		$scope.attractions = ""
+		// put maps in here for now then see how we can try create a seperate controller. 
+		$scope.createMap = function(cityInput){
+			console.log("CITYINPUT:",cityInput )
+			$http.get("https://maps.googleapis.com/maps/api/geocode/json?address="+cityInput+"&sensor=false&key="+GOOGLEGEOCODE.APIKEY).success(function(data){
+				// how does it know which one to GEOCODE??
+				// resolve by putting in cpuntry format in another inputbox as well.
+				// then move on gecoding users position. 
+				console.log("https://maps.googleapis.com/maps/api/geocode/json?address="+cityInput+"&sensor=false&key="+GOOGLEGEOCODE.APIKEY)
+				console.log(data)
+			})
+
+      // if(MAPOPTIONS.map_container != undefined){
+      //   window.map = new google.maps.Map(MAPOPTIONS.map_container, MAPOPTIONS) 
+      //   console.log(window.map)
+      // }
+	  }
 		$scope.submitRequest = function(){
+			console.log("geocode location after request")
+			console.log($scope.city);
 			var query = {attractionType:$scope.attractionType,city:$scope.city}
+			$scope.createMap($scope.city);
 			$scope.savedLocations.push(query)
 			$scope.foursquare(query.attractionType,query.city)
 		}	
@@ -53,12 +92,15 @@ angular.module("myTestAppApp")
 			console.log("query.atttraction",attraction)
 			$http.get(foursquareUrl+"&near="+city+"&query="+attraction+"&venuePhotos=1").success(function(data){
 				$scope.foursquareAttractions = data.response["groups"][0].items
+				// $scope.createMap(); // NEED TO PASS IN LAT POINTS ENTERED FROM USER.
 				console.log($scope.foursquareAttractions);
 				angular.forEach($scope.foursquareAttractions, function(value, key){
-					console.log(value)
-					var savedAttraction = {attractionName:value.venue.name,attractionImagePrefix:value.venue.photos.groups[0].items[0].prefix,attractionImageSuffix:value.venue.photos.groups[0].items[0].suffix}
+					var venue = value.venue;
+
+					var savedAttraction = {attractionName:venue.name,attractionImagePrefix:venue.photos.groups[0].items[0].prefix
+																	,attractionImageSuffix:value.venue.photos.groups[0].items[0].suffix,attractionOpeningTimes:venue.hours?value.venue.hours.status:"not available"}
 					$scope.savedAttractions.push(savedAttraction);
-					// return $scope.foursquareAttractions
+					// $scope.createMap(savedAttraction);
 				});
 				// console.log(data.response["groups"][0].items)
 				console.log($scope.savedAttractions)
